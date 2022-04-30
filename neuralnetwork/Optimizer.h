0eb854c54b5198e6d737ba257ae70dd2e4dfcb32
LilPomidorLil <rozhinthe@ya.ru>
2022-04-30 16:10:39 +0300

    добавлен оптимайзер

--- neuralnetwork/Optimizer.h
+++ neuralnetwork/Optimizer.h
@@ -1,1 +1,29 @@
-#pragma once
+﻿#pragma once
+
+# include <Eigen/Core>
+# include "Config.h"
+
+
+class Optimizer
+{
+protected:
+	typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
+	typedef Vector::ConstAlignedMapType ConstAlignedMapVec;
+	typedef Vector::AlignedMapType AlignedMapVec;
+
+public:
+	virtual ~Optimizer() {}
+
+
+	///
+	/// Сброс информации о текущем оптимайзере
+	/// 
+
+	virtual void reset() {};
+
+	///
+	/// Собственно метод, отвечающий за обновление весов в сетке
+	/// 
+
+	virtual void update(ConstAlignedMapVec& dvec, AlignedMapVec& vec) = 0;
+};
