efefba316ddb4d29d09625cd8b8f3e3a2442c4da
LilPomidorLil <rozhinthe@ya.ru>
2022-04-30 16:16:20 +0300

    добавлен 'полный' коллбэк

---
+++ neuralnetwork/Callback/VerboseCallback.h
@@ -0,0 +1,30 @@
+#pragma once
+
+# include <Eigen/Core>
+# include <iostream>
+# include "../Callback.h"
+# include "../Config.h"
+# include "../NeuralNetwork.h"
+
+
+class VerboseCallback : public Callback
+{
+public:
+	void post_trained_batch(const NeuralNetwork* net,
+		const Matrix& x,
+		const Matrix& y)
+	{
+		const Scalar loss = net->get_output()->loss();
+
+		std::cout << "[Epoch = " << m_epoch_id << ", batch = " << m_batch_id << "] Loss = " << loss << std::endl;
+	}
+
+	void post_trained_batch(const NeuralNetwork* net,
+		const Matrix& x,
+		const IntegerVector& y)
+	{
+		const Scalar loss = net->get_output()->loss();
+
+		std::cout << "[Epoch = " << m_epoch_id << ", batch = " << m_batch_id << "] Loss = " << loss << std::endl;
+	}
+};
