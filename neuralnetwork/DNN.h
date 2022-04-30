bcf1ac1ca11ec1d95a6d095f4adcad19b722cd2e
LilPomidorLil <rozhinthe@ya.ru>
2022-04-30 16:19:46 +0300

    добавлен DNN импортер

--- neuralnetwork/DNN.h
+++ neuralnetwork/DNN.h
@@ -1,1 +1,35 @@
 #pragma once
+
+# include <Eigen/Core>
+
+# include "NeuralNetwork.h"
+
+# include "RNG.h"
+
+# include "Config.h"
+
+
+
+
+# include "Activation/ReLU.h"
+# include "Activation/Sigmoid.h"
+
+
+
+# include "Layer.h"
+# include "Layer/FullyConnected.h"
+
+
+
+# include "Optimizer.h"
+# include "Optimizer/SGD.h"
+
+
+
+# include "Output.h"
+# include "Output/RegressionMSE.h"
+
+
+
+# include "Callback.h"
+# include "Callback/VerboseCallback.h"
