701c692b761a208086a2c5809646a79f6316bc8f
LilPomidorLil <rozhinthe@ya.ru>
2022-04-30 16:17:23 +0300

    добавлен sgd

---
+++ neuralnetwork/Optimizer/SGD.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+# include <Eigen/Core>
+# include "../Config.h"
+# include "../Optimizer.h"
+
+class SGD : public Optimizer
+{
+public:
+	Scalar m_lrate;
+	Scalar m_decay;
+
+	///
+	/// Конструктор по умолчанию, работает всегда. 
+	/// Инициализируется стандартными значениями,
+	/// которые потом пользователь может изменить.
+	/// 
+
+	SGD(const Scalar& lrate = Scalar(0.0001), const Scalar& decay = Scalar(0)) :
+		m_lrate(lrate), m_decay(decay) {}
+
+	void update(ConstAlignedMapVec& dvec, AlignedMapVec& vec)
+	{
+		vec.noalias() -= m_lrate * (dvec + m_decay * vec);
+	}
+};
