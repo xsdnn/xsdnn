49eeee7ccf5f9e1d50ff639da3b4d435590fdfbc
kraken-kryak <rozhinthe@ya.ru>
2022-06-03 22:50:06 +0300

    ++r^2 metrics

--- neuralnetwork/Utils/Metrics.h
+++ neuralnetwork/Utils/Metrics.h
@@ -22,3 +22,17 @@ void MAE_calculate(Matrix& target_data, Matrix& net_output)
   Scalar MAE = (target_data - net_output).array().cwiseAbs().sum() / target_data.cols();
   std::cout << "MAE Error = " << MAE << std::endl;
 }
+
+void R_calculate(Matrix& target_data, Matrix& net_output)
+{
+  Scalar MSE = (target_data - net_output).squaredNorm();
+  Matrix target_data_mean = target_data.rowwise().mean();
+
+  for (int i = 0; i < target_data_mean.size(); i++)
+  {
+    target_data.row(i).array() -= static_cast<Scalar>(target_data_mean(i, 0));
+  }
+  Scalar r_coef = target_data.squaredNorm();
+  std::cout << "R^2 = " << (1 - (MSE / r_coef)) << std::endl;
+  return;
+}
