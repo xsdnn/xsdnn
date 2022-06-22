4b1d789ac04aef66c5931da1499e093701d6a5fb
kraken-kryak <rozhinthe@ya.ru>
2022-06-22 18:24:11 +0300

    изменены подписи --custom callback

--- neuralnetwork/Callback/VerboseCallback.h
+++ neuralnetwork/Callback/VerboseCallback.h
@@ -6,22 +6,20 @@
 # include "../Config.h"
 # include "../NeuralNetwork.h"
 
-
+/*!
+ * \details Класс отображения информации об обучении.
+ */
 class VerboseCallback : public Callback
 {
 public:
+
+    /// Вывод информации в консоль. [ Epoch = ..., batch = ... ] loss = ...
+    /// \param net объекта класса сетки.
+    /// \param x обучающая выборка.
+    /// \param y таргет.
 	void post_trained_batch(const NeuralNetwork* net,
 		const Matrix& x,
 		const Matrix& y) override
-	{
-		const Scalar loss = net->get_output()->loss();
-
-		std::cout << "[Epoch = " << m_epoch_id << ", batch = " << m_batch_id << "] Loss = " << loss << std::endl;
-	}
-
-	void post_trained_batch(const NeuralNetwork* net,
-		const Matrix& x,
-		const IntegerVector& y) override
 	{
 		const Scalar loss = net->get_output()->loss();
 
