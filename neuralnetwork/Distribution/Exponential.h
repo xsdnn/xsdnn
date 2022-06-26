a3671648e5467244958d0b0ac308009da40341a3
kraken-kryak <rozhinthe@ya.ru>
2022-06-26 15:18:38 +0300

    ++распределения

---
+++ neuralnetwork/Distribution/Exponential.h
@@ -0,0 +1,8 @@
+//
+// Created by shuffle on 25.06.22.
+//
+
+#ifndef XSDNN_INCLUDE_EXPONENTIAL_H
+#define XSDNN_INCLUDE_EXPONENTIAL_H
+
+#endif //XSDNN_INCLUDE_EXPONENTIAL_H
---
+++ neuralnetwork/Distribution/Uniform.h
@@ -0,0 +1,8 @@
+//
+// Created by shuffle on 25.06.22.
+//
+
+#ifndef XSDNN_INCLUDE_UNIFORM_H
+#define XSDNN_INCLUDE_UNIFORM_H
+
+#endif //XSDNN_INCLUDE_UNIFORM_H
