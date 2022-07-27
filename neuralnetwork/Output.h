7bb72c85b17d5a8f28658b9fc391bb3659042fd5
kraken-kryak <rozhinthe@ya.ru>
2022-07-28 00:03:44 +0300

    --intvec type

--- neuralnetwork/Output.h
+++ neuralnetwork/Output.h
@@ -14,7 +14,6 @@ class Output
 {
 protected:
     typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
-    typedef Eigen::RowVectorXi IntegerVector;
 
 public:
     virtual ~Output() = default;
@@ -28,16 +27,6 @@ public:
     virtual void check_target_data(const Matrix& target) const {}
 
     /// <summary>
-    /// Отдельный блок проверки для многоклассовой классификации.
-    /// Может не подойти для регрессии поэтому сразу вызовем исключение 
-    /// </summary>
-    /// <param name="target"> - собственно таргет</param>
-    virtual void check_target_data(IntegerVector& target) const
-    {
-        throw std::invalid_argument("[class Output]: This output type cannot take class labels as target data");
-    }
-
-    /// <summary>
     /// Вычисление прямого и обратного распространения для 2 слоев - 
     /// перед выходным и таргетом
     /// </summary>
@@ -45,19 +34,6 @@ public:
     /// <param name="target"> - собственно таргет</param>
     virtual void evaluate(const Matrix& prev_layer_data, const Matrix& target) = 0;
 
-    /// <summary>
-    /// Вычисление прямого и обратного распространения для 2 слоев 
-    /// (Для многоклассовой классификации, !изначально выводится исключение!)
-    ///  - 
-    /// перед выходным и таргетом 
-    /// </summary>
-    /// <param name="prev_layer_data"></param>
-    /// <param name="target"></param>
-    virtual void evaluate(const Matrix& prev_layer_data,
-        const IntegerVector& target)
-    {
-        throw std::invalid_argument("[class Output]: This output type cannot take class labels as target data");
-    }
 
     virtual const Matrix& backprop_data() const = 0;
 
