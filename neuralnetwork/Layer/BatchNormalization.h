61761845e795f0f1825091c657e0be5dd973e93f
kraken-kryak <rozhinthe@ya.ru>
2022-07-29 16:08:11 +0300

    ++init batchnorm

---
+++ neuralnetwork/Layer/BatchNormalization.h
@@ -0,0 +1,16 @@
+//
+// Created by shuffle on 29.07.22.
+//
+
+#ifndef XSDNN_INCLUDE_BATCHNORMALIZATION_H
+#define XSDNN_INCLUDE_BATCHNORMALIZATION_H
+
+
+// TODO: implement batch normalization graph from https://clck.ru/sQxjL
+
+class BatchNorm : public Layer
+{
+
+};
+
+#endif //XSDNN_INCLUDE_BATCHNORMALIZATION_H
