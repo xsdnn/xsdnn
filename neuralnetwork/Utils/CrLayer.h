3715342d431141200ca0d59424e3533d0aefbb73
kraken-kryak <rozhinthe@ya.ru>
2022-08-03 12:12:30 +0300

    meta_info --> Scalar && ++leaky_relu case

--- neuralnetwork/Utils/CrLayer.h
+++ neuralnetwork/Utils/CrLayer.h
@@ -18,6 +18,7 @@
 # include "../Layer/Dropout.h"
 
 # include "../Activation/ReLU.h"
+# include "../Activation/LeakyReLU.h"
 # include "../Activation/Sigmoid.h"
 # include "../Activation/Softmax.h"
 # include "../Activation/Identity.h"
@@ -39,34 +40,57 @@ namespace internal
     /// \param map словарь с информацией о сети
     /// \param index индекс слоя
     /// \return указатель на созданный и _инициализированный_ слой
-    inline Layer* create_layer(const std::map<std::string, int>& map, const int& index)
+    inline Layer* create_layer(const std::map<std::string, Scalar>& map, const int& index)
     {
         std::string ind = std::to_string(index);
-        const int layer_id = map.find("Layer " + ind)->second;
-        const int activation_id = map.find("Activation " + ind)->second;
-        const int distribution_id = map.find("Distribution " + ind)->second;
+        const int layer_id = static_cast<int>(map.find("Layer " + ind)->second);
+        const int activation_id = static_cast<int>(map.find("Activation " + ind)->second);
+        const int distribution_id = static_cast<int>(map.find("Distribution " + ind)->second);
 
         Layer* layer;
 
         if (layer_id == FULLYCONNECTED)
         {
-            const int in_size = map.find("in_size " + ind)->second;
-            const int out_size = map.find("out_size " + ind)->second;
+            const int in_size  = static_cast<int>(map.find("in_size " + ind)->second);
+            const int out_size = static_cast<int>(map.find("out_size " + ind)->second);
+            const int bias     = static_cast<int>(map.find("Bias FC " + ind)->second);
 
             switch (activation_id) {
                 case IDENTITY:
 
                     switch (distribution_id) {
                         case UNIFORM:
-                            layer = new FullyConnected<init::Uniform, activate::Identity>(in_size, out_size);
+                            layer = new FullyConnected<init::Uniform, activate::Identity>(in_size, out_size, bias);
                             break;
 
                         case EXPONENTIAL:
-                            layer = new FullyConnected<init::Exponential, activate::Identity>(in_size, out_size);
+                            layer = new FullyConnected<init::Exponential, activate::Identity>(in_size, out_size, bias);
                             break;
 
                         case NORMAL:
-                            layer = new FullyConnected<init::Normal, activate::Identity>(in_size, out_size);
+                            layer = new FullyConnected<init::Normal, activate::Identity>(in_size, out_size, bias);
+                            break;
+
+                        default:
+                            throw std::invalid_argument("[function create_layer]: Distribution is not of a known type");
+                    }
+                    break;
+
+
+
+                case LEAKYRELU:
+
+                    switch (distribution_id) {
+                        case UNIFORM:
+                            layer = new FullyConnected<init::Uniform, activate::LeakyReLU>(in_size, out_size, bias);
+                            break;
+
+                        case EXPONENTIAL:
+                            layer = new FullyConnected<init::Exponential, activate::LeakyReLU>(in_size, out_size, bias);
+                            break;
+
+                        case NORMAL:
+                            layer = new FullyConnected<init::Normal, activate::LeakyReLU>(in_size, out_size, bias);
                             break;
 
                         default:
@@ -80,15 +104,15 @@ namespace internal
 
                     switch (distribution_id) {
                         case UNIFORM:
-                            layer = new FullyConnected<init::Uniform, activate::ReLU>(in_size, out_size);
+                            layer = new FullyConnected<init::Uniform, activate::ReLU>(in_size, out_size, bias);
                             break;
 
                         case EXPONENTIAL:
-                            layer = new FullyConnected<init::Exponential, activate::ReLU>(in_size, out_size);
+                            layer = new FullyConnected<init::Exponential, activate::ReLU>(in_size, out_size, bias);
                             break;
 
                         case NORMAL:
-                            layer = new FullyConnected<init::Normal, activate::ReLU>(in_size, out_size);
+                            layer = new FullyConnected<init::Normal, activate::ReLU>(in_size, out_size, bias);
                             break;
 
                         default:
@@ -102,15 +126,15 @@ namespace internal
 
                     switch (distribution_id) {
                         case UNIFORM:
-                            layer = new FullyConnected<init::Uniform, activate::Sigmoid>(in_size, out_size);
+                            layer = new FullyConnected<init::Uniform, activate::Sigmoid>(in_size, out_size, bias);
                             break;
 
                         case EXPONENTIAL:
-                            layer = new FullyConnected<init::Exponential, activate::Sigmoid>(in_size, out_size);
+                            layer = new FullyConnected<init::Exponential, activate::Sigmoid>(in_size, out_size, bias);
                             break;
 
                         case NORMAL:
-                            layer = new FullyConnected<init::Normal, activate::Sigmoid>(in_size, out_size);
+                            layer = new FullyConnected<init::Normal, activate::Sigmoid>(in_size, out_size, bias);
                             break;
 
                         default:
@@ -123,15 +147,15 @@ namespace internal
 
                     switch (distribution_id) {
                         case UNIFORM:
-                            layer = new FullyConnected<init::Uniform, activate::Softmax>(in_size, out_size);
+                            layer = new FullyConnected<init::Uniform, activate::Softmax>(in_size, out_size, bias);
                             break;
 
                         case EXPONENTIAL:
-                            layer = new FullyConnected<init::Exponential, activate::Softmax>(in_size, out_size);
+                            layer = new FullyConnected<init::Exponential, activate::Softmax>(in_size, out_size, bias);
                             break;
 
                         case NORMAL:
-                            layer = new FullyConnected<init::Normal, activate::Softmax>(in_size, out_size);
+                            layer = new FullyConnected<init::Normal, activate::Softmax>(in_size, out_size, bias);
                             break;
 
                         default:
@@ -146,10 +170,33 @@ namespace internal
         }
         else if (layer_id == DROPOUT)
         {
-            const int       in_size = map.find("in_size " + ind)->second;
+            const int       in_size = static_cast<int>(map.find("in_size " + ind)->second);
             const Scalar    drop_rate = map.find("dropout_rate " + ind)->second;
 
-            layer = new Dropout(in_size, drop_rate);
+            switch (activation_id) {
+                case IDENTITY:
+                    layer = new Dropout<activate::Identity>(in_size, drop_rate);
+                    break;
+
+                case LEAKYRELU:
+                    layer = new Dropout<activate::LeakyReLU>(in_size, drop_rate);
+                    break;
+
+                case RELU:
+                    layer = new Dropout<activate::ReLU>(in_size, drop_rate);
+                    break;
+
+                case SIGMOID:
+                    layer = new Dropout<activate::Sigmoid>(in_size, drop_rate);
+                    break;
+
+                case SOFTMAX:
+                    layer = new Dropout<activate::Softmax>(in_size, drop_rate);
+                    break;
+
+                default:
+                    throw std::invalid_argument("[function create_layer]: Activation is not of a known type");
+            }
         }
         else
         {
@@ -163,9 +210,9 @@ namespace internal
     /// Автоматическая установка выходного слоя из Meta файла сетки
     /// \param map словарь с информацией о сети.
     /// \return указатель на выходной слой
-    inline Output* create_output(const std::map<std::string, int>& map)
+    inline Output* create_output(const std::map<std::string, Scalar>& map)
     {
-        const int output_id = map.find("OutputLayer")->second;
+        const int output_id = static_cast<int>(map.find("OutputLayer")->second);
         Output* output;
 
         switch (output_id) {
