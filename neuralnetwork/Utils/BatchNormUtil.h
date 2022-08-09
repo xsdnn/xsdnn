72a4ba775f1c470d932f0ec5f6b6af21edd9047b
kraken-kryak <rozhinthe@ya.ru>
2022-08-09 20:11:31 +0300

    ++описание backprop для bn1d

--- neuralnetwork/Utils/BatchNormUtil.h
+++ neuralnetwork/Utils/BatchNormUtil.h
@@ -13,12 +13,17 @@ namespace internal{
     /// \param in исходные входные данные
     /// \param in_hat отнормированные входные данные
     inline void compute_statistic_graph1D_train(
-                const Matrix& in,
-                Matrix& in_hat,
-                Eigen::VectorXd& m_mean,
-                Eigen::VectorXd& m_var,
-                const Scalar& eps,
-                const Scalar& lbd
+                const Matrix&       in,
+                Matrix&             in_hat,
+
+                Eigen::VectorXd&    m_mean,
+                Eigen::VectorXd&    m_var,
+
+                Matrix&             inmu,
+                Eigen::VectorXd&    invvar,
+
+                const Scalar&       eps,
+                const Scalar&       momentum
             )
     {
         const long ncols  = in.cols();
@@ -30,32 +35,33 @@ namespace internal{
         assert(mu.rows() == m_mean.rows());
 
         // вычисление скользящего среднего мат ожидания
-        m_mean = lbd * m_mean + (1 - lbd) * mu;
+        m_mean = momentum * m_mean + (1 - momentum) * mu;
 
-        // compute xmu
-        in_hat = in.colwise() - mu;
+        inmu = in.colwise() - mu;
 
-        Eigen::VectorXd var
-                            =
-                                ( Scalar(1.0) / static_cast<Scalar>(ncols) ) * in_hat.array().square().rowwise().sum();
+        Matrix carre = inmu.array().square();
+
+        Eigen::VectorXd var =  ( Scalar(1.0) / static_cast<Scalar>(ncols) ) * carre.rowwise().sum();
 
         assert(var.cols() == m_var.cols());
         assert(var.rows() == m_var.rows());
 
         // вычисление скользящего среднего дисперсии
-        m_var = lbd * m_var + (1 - lbd) * var;
+        m_var = momentum * m_var + (1 - momentum) * var;
 
-        Eigen::VectorXd denom_var = Scalar(1.0) / (var.array() + eps).array().sqrt();
+        Eigen::VectorXd sqrtvar = (var.array() + eps).array().sqrt();
+
+        invvar = Scalar(1.0) / sqrtvar.array();
 
         // final
-        in_hat = in_hat.array().colwise() * denom_var.array();
+        in_hat = inmu.array().colwise() * invvar.array();
     }
 
     inline void compute_statistic_graph1D_eval(
-            Matrix& in_hat,
-            Eigen::VectorXd& m_mean,
-            Eigen::VectorXd& m_var,
-            const Scalar& eps
+            Matrix&             in_hat,
+            Eigen::VectorXd&    m_mean,
+            Eigen::VectorXd&    m_var,
+            const Scalar&       eps
     )
     {
         // compute xmu
@@ -66,6 +72,46 @@ namespace internal{
 
         // final
         in_hat = in_hat.array().colwise() * denom_var.array();
+    }
+
+    inline void compute_backward_graph1D(
+            const Matrix&       next_layer_data,
+            Matrix&             dLz,
+            Matrix&             m_z,
+            Eigen::VectorXd&    m_gammas,
+
+            Matrix&             m_din,
+            Eigen::VectorXd&    m_dg,
+            Eigen::VectorXd&    m_db,
+
+            Eigen::VectorXd&    invvar,
+            Matrix&             inmu
+            )
+    {
+        Scalar     ncols  = static_cast<Scalar>(m_z.cols());
+
+        m_db = next_layer_data.rowwise().sum();
+        m_dg = (m_z.cwiseProduct(next_layer_data)).rowwise().sum();
+
+        Eigen::VectorXd invvar_square                   = invvar.array().square();
+        Eigen::VectorXd inmu_nld_prod                   = (inmu.cwiseProduct(next_layer_data)).rowwise().sum();
+
+        Eigen::VectorXd gamma_invvar_prod               = m_gammas.cwiseProduct(invvar);
+        Matrix          inmu_invvar_sqr_prod            = inmu.array().colwise() * invvar_square.array();
+        Matrix          inmu_invvar_sqr_inmu_nld_prod   = inmu_invvar_sqr_prod.array().colwise() * inmu_nld_prod.array();
+
+        Matrix tmp_nld  = ncols * next_layer_data;
+
+        tmp_nld         = tmp_nld.colwise() - m_db;
+
+        tmp_nld         = tmp_nld.cwiseProduct(inmu_invvar_sqr_inmu_nld_prod);
+        tmp_nld         = tmp_nld.cwiseProduct(dLz);
+
+        m_din           = tmp_nld.array().colwise() * gamma_invvar_prod.array();
+        m_din.noalias() = (1.0 / ncols) * m_din;
+
+//        m_din = (1.0 / ncols) * m_gammas * invvar *
+//                (ncols * next_layer_data - m_db - inmu * invvar_square * inmu_product_nld) * dLz;
     }
 
 }
