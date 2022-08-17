34e12079654c0576c9b2f12067d0628a66c69bc6
kraken-kryak <rozhinthe@ya.ru>
2022-08-17 21:37:51 +0300

    ++ fc_layer test all passed

---
+++ test/fully_connected_test.h
@@ -0,0 +1,74 @@
+//
+// Created by shuffle on 17.08.22.
+//
+
+// TODO: написать проверку градиента
+
+TEST(fullyconnected, init)
+{
+    Layer* fc_layer = new FullyConnected<init::Normal, activate::Identity>(10, 2, true);
+    std::vector<Scalar> init_params = {0.0, 0.0};
+    RNG rng(1);
+
+    fc_layer->init(init_params, rng);
+    std::vector<Scalar> fc_layer_params = fc_layer->get_parametrs();
+
+    EXPECT_EQ(fc_layer_params.size(), 10 * 2 + 2);
+}
+
+TEST(fullyconnected, init_without_bias)
+{
+    Layer* fc_layer = new FullyConnected<init::Normal, activate::Identity>(10, 2, false);
+    std::vector<Scalar> init_params = {0.0, 0.0};
+    RNG rng(1);
+
+    fc_layer->init(init_params, rng);
+    std::vector<Scalar> fc_layer_params = fc_layer->get_parametrs();
+
+    EXPECT_EQ(fc_layer_params.size(), 10 * 2);
+}
+
+TEST(fullyconnected, forward)
+{
+    Layer* fc_layer = new FullyConnected<init::Constant, activate::Identity>(3, 3, true);
+    std::vector<Scalar> init_params = {1.0};
+    RNG rng(1);
+    fc_layer->init(init_params, rng);
+
+    Matrix in_data(3, 3);
+    in_data <<  1, 1, 1,
+                1, 1, 1,
+                1, 1, 1;
+
+    fc_layer->forward(in_data);
+    Matrix out_data = fc_layer->output();
+
+    Scalar* out_arr = out_data.data();
+    for (int i = 0; i < in_data.size(); i++)
+    {
+        EXPECT_DOUBLE_EQ(Scalar(4.0), out_arr[i]);
+    }
+}
+
+TEST(fullyconnected, forward_without_bias)
+{
+    Layer* fc_layer = new FullyConnected<init::Constant, activate::Identity>(3, 3, false);
+    std::vector<Scalar> init_params = {1.0};
+    RNG rng(1);
+    fc_layer->init(init_params, rng);
+
+    Matrix in_data(3, 3);
+    in_data <<  1, 1, 1,
+                1, 1, 1,
+                1, 1, 1;
+
+    fc_layer->forward(in_data);
+    Matrix out_data = fc_layer->output();
+
+    Scalar* out_arr = out_data.data();
+    for (int i = 0; i < in_data.size(); i++)
+    {
+        EXPECT_DOUBLE_EQ(Scalar(3.0), out_arr[i]);
+    }
+}
+
