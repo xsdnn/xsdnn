0d58ee51b06936c7fe88c0f88a440964fe66c5cb
kraken-kryak <rozhinthe@ya.ru>
2022-08-18 14:12:03 +0300

    ++helper for test

---
+++ test/helper.h
@@ -0,0 +1,66 @@
+//
+// Created by shuffle on 18.08.22.
+//
+
+#ifndef XSDNN_HELPER_H
+#define XSDNN_HELPER_H
+
+template <typename Container>
+bool is_near_container(Container c1, Container c2, Scalar tolerance)
+{
+    auto i1 = std::begin(c1);
+    auto i2 = std::begin(c2);
+
+    for (; i1 != std::end(c1); i1++, i2++)
+    {
+        if (std::abs(*i1 - *i2) > tolerance ) return false;
+    }
+    return true;
+}
+
+bool is_near_container(Matrix m1, Matrix m2, Scalar tolerance)
+{
+    auto i1 = m1.data();
+    auto i2 = m2.data();
+
+    for (; i1 != m1.data() + m1.size(); i1++, i2++)
+    {
+        if (std::abs(*i1 - *i2) > tolerance ) return false;
+    }
+    return true;
+}
+
+
+bool is_equal_container(Matrix m1, Matrix m2)
+{
+    auto i1 = m1.data();
+    auto i2 = m2.data();
+
+    for (; i1 != m1.data() + m1.size(); i1++, i2++)
+    {
+        if (*i1 != *i2) return false;
+    }
+    return true;
+}
+
+
+bool is_different_container(Matrix m1, Matrix m2)
+{
+    auto i1 = m1.data();
+    auto i2 = m2.data();
+
+    for (; i1 !=  m1.data() + m1.size(); i1++, i2++)
+    {
+        if (*i1 != *i2) return true;
+    }
+    return false;
+}
+
+void generate_sinus_data(Matrix& train_image, Matrix& train_label, const Scalar& coef)
+{
+    train_image.setRandom();
+    train_image *= coef;
+    train_label = train_image.array().sin();
+}
+
+#endif //XSDNN_HELPER_H
