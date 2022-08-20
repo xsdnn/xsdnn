f654534c994e9f5879c8032cc4e994ccd3f0411b
kraken-kryak <rozhinthe@ya.ru>
2022-08-21 00:50:00 +0300

    ++mean && varience calc

---
+++ xsDNN/Utils/Math.h
@@ -0,0 +1,43 @@
+//
+// Created by shuffle on 20.08.22.
+//
+
+#ifndef XSDNN_MATH_H
+#define XSDNN_MATH_H
+
+namespace internal {
+    namespace math {
+        typedef Eigen::VectorXd Vector;
+        inline void calc_varience(const Matrix& in,
+                                  const Vector& mu,
+                                  Vector&       var){
+            int index = 0;
+            for (auto row : in.rowwise()){
+                Scalar& curr_var = var[index];
+                const auto it = row.begin();
+                const auto en = row.end();
+                const Scalar mx = mu[index++];
+                curr_var = std::accumulate(it, en, curr_var, [mx](Scalar current, Scalar x){
+                    return current + std::pow(x - mx, Scalar(2.0));
+                });
+                curr_var /= row.size();
+            }
+        }
+
+        inline void update_statistics(const Matrix& in,
+                                      const int&    dim,
+                                      Vector&       mu,
+                                      Vector&       var){
+            mu.resize(dim);
+            assert(mu.size() == in.rows());
+            mu = in.rowwise().mean();
+
+            var.resize(dim);
+            assert(var.size() == in.rows());
+            var.setZero();
+            calc_varience(in, mu, var);
+        }
+    } // end namespace math
+} // end namespace internal
+
+#endif //XSDNN_MATH_H
