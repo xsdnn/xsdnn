8c2f7e65f9adccaca292c80a875defad7d6f6908
shuffle-true <rozhinthe@ya.ru>
2022-09-01 15:26:47 +0300

    ++namespace debug

--- test/helper.h
+++ test/helper.h
@@ -5,66 +5,70 @@
 #ifndef XSDNN_HELPER_H
 #define XSDNN_HELPER_H
 
-template <typename Container>
-bool is_near_container(Container c1, Container c2, Scalar tolerance)
-{
-    auto i1 = std::begin(c1);
-    auto i2 = std::begin(c2);
+namespace internal {
+    namespace debug {
+        template <typename Container>
+        bool is_near_container(Container c1, Container c2, Scalar tolerance)
+        {
+            auto i1 = std::begin(c1);
+            auto i2 = std::begin(c2);
 
-    for (; i1 != std::end(c1); i1++, i2++)
-    {
-        if (std::abs(*i1 - *i2) > tolerance ) return false;
-    }
-    return true;
-}
+            for (; i1 != std::end(c1); i1++, i2++)
+            {
+                if (std::abs(*i1 - *i2) > tolerance ) return false;
+            }
+            return true;
+        }
 
-bool is_near_container(Matrix m1, Matrix m2, Scalar tolerance)
-{
-    auto i1 = m1.data();
-    auto i2 = m2.data();
+        bool is_near_container(Matrix m1, Matrix m2, Scalar tolerance)
+        {
+            auto i1 = m1.data();
+            auto i2 = m2.data();
 
-    for (; i1 != m1.data() + m1.size(); i1++, i2++)
-    {
-        if (std::abs(*i1 - *i2) > tolerance ) return false;
-    }
-    return true;
-}
-
-
-bool is_equal_container(Matrix m1, Matrix m2)
-{
-    auto i1 = m1.data();
-    auto i2 = m2.data();
-
-    for (; i1 != m1.data() + m1.size(); i1++, i2++)
-    {
-        if (*i1 != *i2) return false;
-    }
-    return true;
-}
+            for (; i1 != m1.data() + m1.size(); i1++, i2++)
+            {
+                if (std::abs(*i1 - *i2) > tolerance ) return false;
+            }
+            return true;
+        }
 
 
-bool is_different_container(Matrix m1, Matrix m2)
-{
-    auto i1 = m1.data();
-    auto i2 = m2.data();
+        bool is_equal_container(Matrix m1, Matrix m2)
+        {
+            auto i1 = m1.data();
+            auto i2 = m2.data();
 
-    for (; i1 !=  m1.data() + m1.size(); i1++, i2++)
-    {
-        if (*i1 != *i2) return true;
-    }
-    return false;
-}
-
-void generate_sinus_data(Matrix& train_image, Matrix& train_label, const Scalar& coef)
-{
-    train_image.setRandom();
-    train_image *= coef;
-    train_label = train_image.array().sin();
-}
+            for (; i1 != m1.data() + m1.size(); i1++, i2++)
+            {
+                if (*i1 != *i2) return false;
+            }
+            return true;
+        }
 
 
-Scalar epsilon      = std::numeric_limits<Scalar>::epsilon();
-Scalar sqrt_epsilon = std::sqrt(std::numeric_limits<Scalar>::epsilon());
+        bool is_different_container(Matrix m1, Matrix m2)
+        {
+            auto i1 = m1.data();
+            auto i2 = m2.data();
+
+            for (; i1 !=  m1.data() + m1.size(); i1++, i2++)
+            {
+                if (*i1 != *i2) return true;
+            }
+            return false;
+        }
+
+        void generate_sinus_data(Matrix& train_image, Matrix& train_label, const Scalar& coef)
+        {
+            train_image.setRandom();
+            train_image *= coef;
+            train_label = train_image.array().sin();
+        }
+
+
+        Scalar epsilon      = std::numeric_limits<Scalar>::epsilon();
+        Scalar sqrt_epsilon = std::sqrt(std::numeric_limits<Scalar>::epsilon());
+    } // end namespace debug
+} // end namespace internal
 
 #endif //XSDNN_HELPER_H
