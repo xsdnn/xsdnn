3792dc917d4b210e87af990af8f9c830507a5618
shuffle-true <rozhinthe@ya.ru>
2022-10-11 22:52:53 +0300

    ++num_core func and THread device definition

--- xsDNN/Config.h
+++ xsDNN/Config.h
@@ -5,6 +5,14 @@
 #ifndef XSDNN_CONFIG_H
 #define XSDNN_CONFIG_H
 
+#if defined(_WIN32)
+#include <windows.h>
+#elif defined(__linux__) || defined(unix)
+#include <unistd.h>
+#else
+printf("Unsupported OS");
+exit(1);
+#endif
 
 namespace xsdnn {
     typedef double Scalar;
@@ -26,12 +34,23 @@ namespace xsdnn {
     };
 } // namespace xsdnn
 
-// TODO: доделать поточную структуру с пулом и девайсом
 namespace xsdnn {
-    struct xsThread {
-        sysconf(_SC_NPROCESSORS_ONLN);
-        Eigen::ThreadPool pool()
-    };
+    namespace internal {
+        int64_t num_core() {
+#if defined(_WIN32)
+            SYSTEM_INFO sysinfo;
+            GetSystemInfo(&sysinfo);
+            return sysinfo.dwNumberOfProcessors;
+#elif defined(__linux__) || defined(unix)
+            return sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+        }
+    } // namespace internal
+
+    namespace xsThread {
+        Eigen::ThreadPool pool(internal::num_core());
+        Eigen::ThreadPoolDevice CPUDevice(&pool, pool.NumThreads());
+    } // namespace xsThread
 } // namespace xsdnn
 
 #endif //XSDNN_CONFIG_H
