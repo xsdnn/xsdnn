095398fc4669a76838e2b6998685697d50df4147
shuffle-true <rozhinthe@ya.ru>
2022-10-15 00:14:59 +0300

    ++ vanilla cmake.txt with test

---
+++ test/test.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Andrei R. on 14.10.22.
+// Copyright (c) 2022 xsDNN. All rights reserved.
+//
+
+#include "test.h"
---
+++ test/type_test.h
@@ -0,0 +1,9 @@
+//
+// Created by Andrei R. on 14.10.22.
+// Copyright (c) 2022 xsDNN. All rights reserved.
+//
+
+#ifndef XSDNN_TYPE_TEST_H
+#define XSDNN_TYPE_TEST_H
+
+#endif //XSDNN_TYPE_TEST_H
