50a05a8db3ce8c1561ea18b8877bbd3ad9d47945
shuffle-true <rozhinthe@ya.ru>
2022-10-15 00:16:29 +0300

    ++config with device and tensor types low and up level API

--- src/config.h
+++ src/config.h
@@ -6,9 +6,57 @@
 #ifndef XSDNN_CONFIG_H
 #define XSDNN_CONFIG_H
 
+
+#if defined(_WIN32)
+#include <windows.h>
+#elif defined(__linux__) || defined(unix)
+#include <unistd.h>
+#else
+printf("Unsupported OS");
+exit(1);
+#endif
+
 #if defined(DNN_USE_DOUBLE)
 typedef double Scalar;
 #else
 typedef float Scalar;
+#endif
+
+namespace xsdnn {
+    struct xsTypes {
+        typedef Eigen::TensorMap<Eigen::Tensor<Scalar, 4, Eigen::ColMajor, Eigen::DenseIndex>, Eigen::Aligned>
+                AlignedTensor_4D;
+        typedef Eigen::TensorMap<Eigen::Tensor<Scalar, 3, Eigen::ColMajor, Eigen::DenseIndex>, Eigen::Aligned>
+                AlignedTensor_3D;
+        typedef Eigen::TensorMap<Eigen::Tensor<Scalar, 2, Eigen::ColMajor, Eigen::DenseIndex>, Eigen::Aligned>
+                AlignedMatrix;
+        typedef Eigen::TensorMap<Eigen::Tensor<Scalar, 1, Eigen::ColMajor, Eigen::DenseIndex>, Eigen::Aligned>
+                AlignedVector;
+    };
+
+    // use for up-level API
+    typedef Eigen::Tensor<Scalar, 4, Eigen::ColMajor, Eigen::DenseIndex>
+            Tensor_4D;
+    typedef Eigen::Tensor<Scalar, 3, Eigen::ColMajor, Eigen::DenseIndex>
+            Tensor_3D;
+    typedef Eigen::Tensor<Scalar, 2, Eigen::ColMajor, Eigen::DenseIndex>
+            Matrix;
+    typedef Eigen::Tensor<Scalar, 1, Eigen::ColMajor, Eigen::DenseIndex>
+            Vector;
+
+    namespace xsThread {
+        int64_t num_core() {
+#if defined(_WIN32)
+            SYSTEM_INFO sysinfo;
+            GetSystemInfo(&sysinfo);
+            return sysinfo.dwNumberOfProcessors;
+#elif defined(__linux__) || defined(unix)
+            return sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+        }
+        Eigen::ThreadPool pool(num_core());
+        Eigen::ThreadPoolDevice cpu(&pool, pool.NumThreads());
+    } // namespace xsThread
+} // namespace xsdnn
 
 #endif //XSDNN_CONFIG_H
