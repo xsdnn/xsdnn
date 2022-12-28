98237892c63cdd903a4d38d411c0d404454a56b0
shuffle-true <rozhinthe@ya.ru>
2022-12-29 01:03:13 +0300

    Implement xs_error && xs_warning

--- src/util/xs_error.hpp
+++ src/util/xs_error.hpp
@@ -6,4 +6,31 @@
 #ifndef XSDNN_XS_ERROR_HPP
 #define XSDNN_XS_ERROR_HPP
 
+#include <exception>
+
+namespace xsdnn {
+
+class xs_error : std::exception {
+public:
+    xs_error(const std::string& msg) : msg_(msg) {}
+    const char* what() const throw() override {return msg_.c_str();}
+
+private:
+    std::string msg_;
+};
+
+class xs_warning {
+public:
+    explicit xs_warning(const std::string& msg) : msg_(msg) {
+        std::cout << cc::red << type_ + msg_ << std::endl;
+    }
+
+private:
+    std::string msg_;
+    std::string type_ = "[WARNING]";
+};
+
+} // xsdnn
+
+
 #endif //XSDNN_XS_ERROR_HPP
