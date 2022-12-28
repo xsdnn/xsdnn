237690611c297bc99b04727fb241e362d1a875c9
shuffle-true <rozhinthe@ya.ru>
2022-12-29 01:59:12 +0300

    Add edge_node_test file

---
+++ test/edge_node_test.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Andrei R. on 29.12.22.
+// Copyright (c) 2022 xsDNN. All rights reserved.
+//
+
+#ifndef XSDNN_EDGE_NODE_TEST_HPP
+#define XSDNN_EDGE_NODE_TEST_HPP
+
+#endif //XSDNN_EDGE_NODE_TEST_HPP
