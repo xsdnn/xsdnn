0983b7012397cd7f84e6a155fdb914016d38a9e5
shuffle-true <rozhinthe@ya.ru>
2022-12-29 00:49:31 +0300

    Add colored print

---
+++ src/util/color_print.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Andrei R. on 29.12.22.
+// Copyright (c) 2022 xsDNN. All rights reserved.
+//
+
+#ifndef XSDNN_COLOR_PRINT_HPP
+#define XSDNN_COLOR_PRINT_HPP
+
+#endif //XSDNN_COLOR_PRINT_HPP
---
+++ src/util/xs_error.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Andrei R. on 29.12.22.
+// Copyright (c) 2022 xsDNN. All rights reserved.
+//
+
+#ifndef XSDNN_XS_ERROR_HPP
+#define XSDNN_XS_ERROR_HPP
+
+#endif //XSDNN_XS_ERROR_HPP
