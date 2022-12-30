170e0b84b81ce9aa1f2aceba295cecaf20ca9788
shuffle-true <rozhinthe@ya.ru>
2022-12-30 20:44:18 +0300

    Add debug main executable

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Andrei R. on 30.12.22.
+// Copyright (c) 2022 xsDNN. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include "xsDNN.hpp"
+using json = nlohmann::json;
+
+int main() {
+    xsdnn::io::make_dir("FOLDER_TO");
+}
