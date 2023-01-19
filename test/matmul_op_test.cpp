856dc6a25e2191466e6d75d7e2fc68111a6ae7f1
shuffle-true <rozhinthe@ya.ru>
2023-01-20 01:27:39 +0300

    Impl test for real matmul with reduce op

--- test/matmul_op_test.cpp
+++ test/matmul_op_test.cpp
@@ -27,6 +27,28 @@ TEST(matmul, vanill) {
     ASSERT_EQ(Xw(1, 0), 28);
 }
 
+TEST(matmul, reduce) {
+    Tensor_3D X(1, 2, 3);
+    X.setValues({ {{1, 2, 2}, {3, 4, 2}} });
+    Tensor_3D w(1, 3, 1);
+    w.setValues({ {{4}, {4}, {4}} });
+
+    Tensor_3D Xw(1, 2, 1);  // result tensor out
+
+    ReduceArray1D dim_reduce = {2};
+    tensorize::matmul(xsThread::cpu, X, w, Xw, {DimPair(2, 1)}, dim_reduce);
+
+    auto dim = Xw.dimensions();
+
+    ASSERT_EQ(dim.size(), 3);
+    ASSERT_EQ(dim[0], 1);
+    ASSERT_EQ(dim[1], 2);
+    ASSERT_EQ(dim[2], 1);
+
+    ASSERT_EQ(Xw(0, 0, 0), 20);
+    ASSERT_EQ(Xw(0, 1, 0), 36);
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
