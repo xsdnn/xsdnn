2ec243760f6b2b2cf7c07aa6ab4e86a67e026ed5
shuffle-true <rozhinthe@ya.ru>
2023-01-20 01:27:14 +0300

    Add real matmul with reduce op

--- src/util/math.hpp
+++ src/util/math.hpp
@@ -10,25 +10,32 @@ namespace xsdnn {
 
 using DimPair = Eigen::IndexPair<Eigen::DenseIndex>;
 using DimArray = std::array<DimPair, 1>;
+using ReduceArray1D = std::array<Eigen::DenseIndex, 1>;
+using ReduceArray2D = std::array<Eigen::DenseIndex, 2>;
 
 // must be only static member funtion class
 class tensorize {
 public:
+
+    /*
+     * Classical matmul-op
+     */
     template<typename Device, typename In1, typename In2, typename Out>
-    static void matmul(Device d, In1& in1, In2& in2, Out& out, const DimArray& dim) {
+    static void matmul(Device& d, In1& in1, In2& in2, Out& out, const DimArray& dim) {
         out.device(d) = in1.contract(in2, dim);
+    }
+
+    /*
+     * Matmul + reduce op
+     */
+    template<typename Device, typename In1, typename In2, typename Out,
+             typename DimReduce>
+    static void matmul(Device& d, In1& in1, In2& in2, Out& out,
+                       const DimArray& dim, const DimReduce& dim_reduce) {
+        out.device(d) = in1.contract(in2, dim).maximum(dim_reduce);
     }
 };
 
 } // xsdnn
-
-// TODO: impl MatMul via static method of some functor?
-
-// TODO: impl MatMul with allocate check
-
-
-
-
-
 
 #endif //XSDNN_MATH_HPP
