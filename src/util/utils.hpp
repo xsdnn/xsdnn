24c71b91b8ae109fdedc375227d7fdd0ecce1f2a
shuffle-true <rozhinthe@ya.ru>
2023-01-25 22:44:28 +0300

    Delete unused methods

--- src/util/utils.hpp
+++ src/util/utils.hpp
@@ -106,41 +106,11 @@ bool is_trainable_concept(tensor_type type_) {
     return value;
 }
 
-void tensor_fill(Scalar* data, size_t size, Scalar value) {
-    for (size_t i = 0; i < size; ++i) {
-        data[i] = value;
-    }
-}
 
+// FIXME: can we do better at this place?
 Matrix transpose(Matrix& m) {
     auto shuffle_idx = std::array<Eigen::DenseIndex, 2> {1, 0};
     return m.shuffle(shuffle_idx);
-}
-
-
-Tensor_4D _3D_to_4D(std::array<Eigen::DenseIndex, 3> copies, /*batch_size, height, width*/
-                    std::array<Eigen::DenseIndex, 4> shape,  /*batch_size, channel, height, width*/
-                    Tensor_3D& t_) {
-    return t_.broadcast(copies).reshape(shape);
-}
-
-/*
- * This method can transform 4D Tensor to 2D ColMajor Matrix if shuffle & shape param correct
- *
- * >>> Tranform 4D(60, 1, 28, 1) -> 2D(28, 60)
- * >>> shuffle_ = {2, 0, 1, 3}
- * >>> shape_   = {28, 60}
- */
-Matrix _4D_to_2D(std::array<Eigen::DenseIndex, 4> shuffle_,
-                 std::array<Eigen::DenseIndex, 2> shape_,
-                 Tensor_4D& t_) {
-    return t_.shuffle(shuffle_).reshape(shape_);
-}
-
-Matrix _3D_to_2D(std::array<Eigen::DenseIndex, 3> shuffle_,
-                std::array<Eigen::DenseIndex, 2> shape_,
-                Tensor_3D& t_) {
-    return t_.shuffle(shuffle_).reshape(shape_);
 }
 
 } // xsdnn
