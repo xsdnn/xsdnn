18d4031f533f81e30591404333f91aa10e49bc58
Andrew Rozhin <rozhinthe@ya.ru>
2023-03-31 22:35:19 +0300

    Define vanilla structure

---
+++ src/network.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Andrei R. on 31.03.23.
+// Copyright (c) 2023 xsDNN. All rights reserved.
+//
+
+#ifndef XSDNN_NETWORK_HPP
+#define XSDNN_NETWORK_HPP
+
+#endif //XSDNN_NETWORK_HPP
---
+++ src/node.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Andrei R. on 31.03.23.
+// Copyright (c) 2023 xsDNN. All rights reserved.
+//
+
+#ifndef XSDNN_NODE_HPP
+#define XSDNN_NODE_HPP
+
+#endif //XSDNN_NODE_HPP
