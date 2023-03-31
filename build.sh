e39270d588dd2b6460a09033d499c8e65894d6e1
Andrew Rozhin <rozhinthe@ya.ru>
2023-03-31 23:20:25 +0300

    Impl tiny build system

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OS=$(uname -s)
 
-python3 $DIR/tools/ci_build/build.py
+if [ "$OS" == "Linux" ]; then
+    DIR_OS="Linux"
+else
+    echo "Unsupported system!"
+    exit 1
+fi
+
+python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build/$DIR_OS
--- tools/ci_build/build.py
+++ tools/ci_build/build.py
@@ -1,7 +1,36 @@
+import os
+import argparse
 import subprocess
 
-def test_func():
-    print("Hello World")
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+            "--build_dir", 
+            required=True, 
+            help="Path to the build directory"        
+    )
+
+    parser.add_argument(
+            "--skip_submodule_sync",
+            action='store_true',
+            help="Don't do a 'git submodule update'. Makes the Update phase faster."
+    )
+
+    return parser.parse_args()
+
+def update_submodules(source_dir):
+    subprocess.run(["git", "submodule", "sync", "--recursive"])
+    subprocess.run(["git", "submodule", "update", "--init", "--recursive"])
+
+def main():
+    args = parse_arguments()
+    script_dir = os.path.realpath(os.path.dirname(__file__))
+    source_dir = os.path.normpath(os.path.join(script_dir, "..", ".."))
+
+    if not args.skip_submodule_sync:
+        update_submodules(source_dir)
+
 
 if __name__ == '__main__':
-    test_func()
+    main()
