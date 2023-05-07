e059f24294ca1452343f8d3ec08abce8885a4e38
akrozhin <akrozhin@miem.hse.ru>
2023-05-07 23:04:01 +0300

    Add sgd impl

--- cmake/xsdnn_optimizers.cmake
+++ cmake/xsdnn_optimizers.cmake
@@ -2,6 +2,9 @@ set(
         xsdnn_optimizers_src
         "${XSROOT_SRC}/optimizers/optimizer_base.h"
         "${XSROOT_SRC}/optimizers/optimizer_base.cc"
+        "${XSROOT_SRC}/optimizers/sgd.h"
+        "${XSROOT_SRC}/optimizers/sgd.cc"
+
 )
 
 add_library(xsdnn_optimizers STATIC ${xsdnn_optimizers_src})
