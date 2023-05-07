255c4a90c590509a2a16f6e611f5a116a1153077
akrozhin <akrozhin@miem.hse.ru>
2023-05-07 23:06:02 +0300

    Small changies like trransfer files

--- src/mmpack/inc/mmpack.h
+++ src/mmpack/inc/mmpack.h
@@ -1,2 +1,4 @@
 #include "../lib/utils/allocator.h"
-#include "../lib/sgemm.h"
+#include "../lib/gemm.h"
+#include "../lib/v2mm.h"
+#include "../lib/dot.h"
--- src/mmpack/lib/gemm/sgemm.cc
+++ src/mmpack/lib/gemm/sgemm.cc
@@ -3,7 +3,7 @@
 // Copyright (c) 2021-2023 xsdnn. All rights reserved.
 //
 
-#include "../sgemm.h"
+#include "../gemm.h"
 
 namespace mmpack {
 
