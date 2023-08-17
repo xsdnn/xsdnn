d060c4f42603a73a348cbd786195c70d8c2015a4
akrozhin <akrozhin@miem.hse.ru>
2023-08-17 15:18:26 +0300

    Impl test for reshape method

---
+++ test/test_reshape.cc
@@ -0,0 +1,16 @@
+//
+// Created by rozhin on 17.08.2023.
+// Copyright (c) 2021-2023 xsdnn. All rights reserved.
+//
+
+#include "xsdnn.h"
+#include <gtest/gtest.h>
+#include "test_utils.h"
+using namespace xsdnn;
+
+
+TEST(reshape, cerial) {
+    shape3d shape_ = shape3d(3, 224, 224);
+    reshape res(shape_);
+    ASSERT_TRUE(utils::cerial_testing(res));
+}
